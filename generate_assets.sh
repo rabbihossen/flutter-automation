89ad13857d516ea57c132a7c17a9496ae411c4aa
Rabbi Hossen <97307036+rabbihossen@users.noreply.github.com>
2025-02-01 20:16:27 +0600

    Update generate_assets.sh

--- generate_assets.sh
+++ generate_assets.sh
@@ -21,7 +21,86 @@ echo "  static const \$Images images = \$Images();" >>$output_file
 echo "}" >>$output_file
 echo "" >>$output_file
 
-# Function to generate class for a folder
+# Include AssetGen class definition
+echo "class AssetGen {" >>$output_file
+echo "  const AssetGen(this._assetName, {this.size, this.flavors = const {}});" >>$output_file
+echo "" >>$output_file
+echo "  final String _assetName;" >>$output_file
+echo "" >>$output_file
+echo "  final Size? size;" >>$output_file
+echo "  final Set<String> flavors;" >>$output_file
+echo "" >>$output_file
+echo "  Image image({" >>$output_file
+echo "    Key? key," >>$output_file
+echo "    AssetBundle? bundle," >>$output_file
+echo "    ImageFrameBuilder? frameBuilder," >>$output_file
+echo "    ImageErrorWidgetBuilder? errorBuilder," >>$output_file
+echo "    String? semanticLabel," >>$output_file
+echo "    bool excludeFromSemantics = false," >>$output_file
+echo "    double? scale," >>$output_file
+echo "    double? width," >>$output_file
+echo "    double? height," >>$output_file
+echo "    Color? color," >>$output_file
+echo "    Animation<double>? opacity," >>$output_file
+echo "    BlendMode? colorBlendMode," >>$output_file
+echo "    BoxFit? fit," >>$output_file
+echo "    AlignmentGeometry alignment = Alignment.center," >>$output_file
+echo "    ImageRepeat repeat = ImageRepeat.noRepeat," >>$output_file
+echo "    Rect? centerSlice," >>$output_file
+echo "    bool matchTextDirection = false," >>$output_file
+echo "    bool gaplessPlayback = false," >>$output_file
+echo "    bool isAntiAlias = false," >>$output_file
+echo "    String? package," >>$output_file
+echo "    FilterQuality filterQuality = FilterQuality.low," >>$output_file
+echo "    int? cacheWidth," >>$output_file
+echo "    int? cacheHeight," >>$output_file
+echo "  }) {" >>$output_file
+echo "    return Image.asset(" >>$output_file
+echo "      _assetName," >>$output_file
+echo "      key: key," >>$output_file
+echo "      bundle: bundle," >>$output_file
+echo "      frameBuilder: frameBuilder," >>$output_file
+echo "      errorBuilder: errorBuilder," >>$output_file
+echo "      semanticLabel: semanticLabel," >>$output_file
+echo "      excludeFromSemantics: excludeFromSemantics," >>$output_file
+echo "      scale: scale," >>$output_file
+echo "      width: width," >>$output_file
+echo "      height: height," >>$output_file
+echo "      color: color," >>$output_file
+echo "      opacity: opacity," >>$output_file
+echo "      colorBlendMode: colorBlendMode," >>$output_file
+echo "      fit: fit," >>$output_file
+echo "      alignment: alignment," >>$output_file
+echo "      repeat: repeat," >>$output_file
+echo "      centerSlice: centerSlice," >>$output_file
+echo "      matchTextDirection: matchTextDirection," >>$output_file
+echo "      gaplessPlayback: gaplessPlayback," >>$output_file
+echo "      isAntiAlias: isAntiAlias," >>$output_file
+echo "      package: package," >>$output_file
+echo "      filterQuality: filterQuality," >>$output_file
+echo "      cacheWidth: cacheWidth," >>$output_file
+echo "      cacheHeight: cacheHeight," >>$output_file
+echo "    );" >>$output_file
+echo "  }" >>$output_file
+echo "" >>$output_file
+echo "  ImageProvider provider({" >>$output_file
+echo "    AssetBundle? bundle," >>$output_file
+echo "    String? package," >>$output_file
+echo "  }) {" >>$output_file
+echo "    return AssetImage(" >>$output_file
+echo "      _assetName," >>$output_file
+echo "      bundle: bundle," >>$output_file
+echo "      package: package," >>$output_file
+echo "    );" >>$output_file
+echo "  }" >>$output_file
+echo "" >>$output_file
+echo "  String get path => _assetName;" >>$output_file
+echo "" >>$output_file
+echo "  String get keyName => _assetName;" >>$output_file
+echo "}" >>$output_file
+echo "" >>$output_file
+
+# Function to generate a class for a folder
 generate_class() {
     local class_name=$1
     local folder=$2
@@ -30,7 +109,7 @@ generate_class() {
     echo "  const $class_name();" >>$output_file
 
     # Loop through all files and create getters for them.
-    for file in $folder/*; do
+    for file in "$folder"/*; do
         # Extract file name without extension.
         filename=$(basename -- "$file")
         filename_noext="${filename%.*}"
@@ -50,7 +129,7 @@ generate_class() {
 
     # Add the list of values.
     echo "  List<dynamic> get values => [" >>$output_file
-    for file in $folder/*; do
+    for file in "$folder"/*; do
         filename=$(basename -- "$file")
         filename_noext="${filename%.*}"
         camel_case_name=$(echo $filename_noext | sed -r 's/(_[a-z])/\U\1/g' | sed 's/_//g')
